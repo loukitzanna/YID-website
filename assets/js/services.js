47800e68ac0e0f5e86fb326fc93093a4d25b7352
Justin Yang <justinyang@email.arizona.edu>
2015-01-31 12:29:48 -0700

    added services functionality

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,4 +1,30 @@
 $(function(){
-
-
+	$("#btnsale").click(function(){
+		resetclick();
+		$(".rent").css("display", "block");
+	});
+	$("#btnphoto").click(function(){
+		resetclick();
+		$(".photo").css("display", "block");
+	});
+	$("#btnbeauty").click(function(){
+		resetclick();
+		$(".face").css("display", "block");
+	});
+	$("#btnfloral").click(function(){
+		resetclick();
+		$(".floral").css("display", "block");
+	});
+	$("#btnorder").click(function(){
+		resetclick();
+		$(".print").css("display", "block");
+	});
 });
+
+resetclick = function(){
+	$(".rent").css("display", "none");
+	$(".photo").css("display", "none");
+	$(".floral").css("display", "none");
+	$(".face").css("display", "none");
+	$(".print").css("display", "none");
+}
